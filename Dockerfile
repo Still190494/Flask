c75b3d08a26b4fbaf8c2e8833a0c5ba0e64f664f
Still190494 <Still190494@ya.ru>
2023-03-18 20:15:32 +0300

    migrations

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
-FROM python:3.9.1-buster
+FROM python:3.10.2
 
 WORKDIR /app
 
+RUN python -m pip install --upgrade pip
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
+
 COPY wsgi.py wsgi.py
 COPY blog ./blog
+
+RUN flask init-db
+RUN flask create-users
 
 EXPOSE 5000
 
--- blog/app.py
+++ blog/app.py
@@ -8,18 +8,22 @@ from werkzeug.exceptions import BadRequest
 from blog.models.database import db
 from blog.views.auth import login_manager, auth_app
 import os
-
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////blog.db"
+file_path = os.path.abspath(os.getcwd()) + "\database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////blog.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "abcdefg123456"
 db.init_app(app)
 login_manager.init_app(app)
 
 
-cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
+migrate = Migrate(app, db, compare_type=True)
+
+cfg_name = os.environ.get("CONFIG_NAME") or "DevConfig"
 app.config.from_object(f"blog.configs.{cfg_name}")
 
 
@@ -102,4 +106,3 @@ def handle_zero_division_error(error):
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
 app.register_blueprint(auth_app, url_prefix="/auth")
-
