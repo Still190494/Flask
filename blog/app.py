2b37750321d9e5dffe4ce14faa6c3f4b97779ee4
Still190494 <100068017+Still190494@users.noreply.github.com>
2023-03-18 09:46:03 +0300

    Lesson 3 (#3)
    * create user_model and SQLAlchemy
    
    * create auth
    
    * lesson_3

--- blog/app.py
+++ blog/app.py
@@ -5,8 +5,19 @@ from flask import Flask, render_template
 from flask import request
 from flask import g
 from werkzeug.exceptions import BadRequest
+from blog.models.database import db
+from blog.views.auth import login_manager, auth_app
+
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "abcdefg123456"
+db.init_app(app)
+login_manager.init_app(app)
+
+
 
 @app.route("/")
 def index():
@@ -86,3 +97,5 @@ def handle_zero_division_error(error):
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.register_blueprint(auth_app, url_prefix="/auth")
+
