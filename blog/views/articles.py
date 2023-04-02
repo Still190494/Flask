afdc0fc622fe44623339c8462fd388fdfe975f4f
Still190494 <100068017+Still190494@users.noreply.github.com>
2023-04-02 16:39:31 +0300

    lesson_7 (#7)

--- blog/views/articles.py
+++ blog/views/articles.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request, current_app, redirect, url_for
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 from blog.models.database import db
 from blog.models import Author, Article
 from blog.forms.article import CreateArticleForm
+from blog.models.tag import Tag
 
 
 articles_app = Blueprint("articles_app", __name__)
@@ -16,7 +18,10 @@ def articles_list():
 
 @articles_app.route("/<int:article_id>/", endpoint="details")
 def article_detals(article_id):
-    article = Article.query.filter_by(id=article_id).one_or_none()
+    article = Article.query.filter_by(id=article_id).options(
+        joinedload(Article.tags) # подгружаем связанные теги!
+    ).one_or_none()
+
     if article is None:
         raise NotFound
     return render_template("articles/details.html", article=article)
@@ -27,8 +32,13 @@ def article_detals(article_id):
 def create_article():
     error = None
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by("name")]
     if request.method == "POST" and form.validate_on_submit():
         article = Article(title=form.title.data.strip(), body=form.body.data)
+        if form.tags.data: # если в форму были переданы теги (были выбраны)
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                article.tags.append(tag) # добавляем выбранные теги к статье
         if current_user.author:
             # use existing author if present
             article.author = current_user.author
